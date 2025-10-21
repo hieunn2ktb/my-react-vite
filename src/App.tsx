c2ccdbf2bc1594b5e769c43daa2cf984522be090
hieunn2ktb <choilacket2000@gmail.com>
2025-10-21 22:57:33 +0700

    bai33

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './conponents/todo/todo.css';
+import TodoData from './conponents/todo/todoData';
+import TodoNew from './conponents/todo/todoNew';
+import reactLogo from './assets/react.svg'
+
 function App() { 
-  
-   
   return (
     <>
       <div className="toto-container">
         <div className="todo-title"> Todo List</div>
-        <div>
-          <input type="text" />
-          <button>Add</button>
-        </div>
-        <div>
-          <div>learning React</div>
+        <TodoNew/>
+        <TodoData/>
+        <div className='todo-image'>
+        <img src={reactLogo} />
         </div>
       </div>
       
---
+++ src/conponents/todo/todoData.tsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+    return (
+        <div className="todo-data">
+          <div>learning React</div>
+            <div>learning Vite</div>
+        </div>
+    );
+}
+
+export default TodoData;
