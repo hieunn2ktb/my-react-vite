cb567ff3acd6510b83247532410ae6708f11d4de
hieunn <hieu.nn@infinq>
2025-10-29 23:14:52 +0700

    hieu update

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,6 +1,12 @@
+import UserForm from "../conponents/user/user.form";
+import UserTable from "../conponents/user/user.table";
+
 const UserPage = () => {
     return (
-        <div>User Page</div>
+        <div style={{padding: "20px"}}>
+            <div><UserForm/></div>
+            <div><UserTable/></div>
+        </div>
     )
 }
 
